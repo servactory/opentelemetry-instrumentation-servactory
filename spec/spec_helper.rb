6589b5f8353d62b9eee39b4a5b5c0fb8d83a160e
Anton Sokolov <profox.rus@gmail.com>
2026-02-21 00:18:50 +0300

    Initialize OpenTelemetry SDK and configure RSpec for tracing
    **Summary of changes:**
    
    - Added OpenTelemetry SDK setup with an in-memory span exporter.
    - Configured RSpec to reset the exporter before each test run.
    - Required supporting files under the `support` directory automatically.
    - Set up OpenTelemetry instrumentation and error handling for Servactory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require "forwardable"
+require "servactory"
 require "opentelemetry/instrumentation/servactory"
+require "opentelemetry-sdk"
+
+Dir[File.join(__dir__, "support", "**", "*.rb")].each { |f| require f }
+
+EXPORTER = OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -17,4 +24,14 @@ RSpec.configure do |config|
     # doing truncation.
     c.max_formatted_output_length = nil
   end
+
+  config.before do
+    EXPORTER.reset
+  end
+end
+
+OpenTelemetry::SDK.configure do |c|
+  c.error_handler = ->(exception:, message:) { raise(exception || message) }
+  c.add_span_processor(OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(EXPORTER))
+  c.use("OpenTelemetry::Instrumentation::Servactory")
 end
