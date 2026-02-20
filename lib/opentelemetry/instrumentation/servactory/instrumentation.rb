9fe244be718dfa502f0ed4ce51ff777da320c515
Anton Sokolov <profox.rus@gmail.com>
2026-02-21 00:01:19 +0300

    Add compatibility check and patching logic for Servactory
    **Summary of changes:**
    
    - Introduced `compatible` block to validate minimum required Servactory version (`>= 2.16.0`).
    - Added configuration options (`trace_actions`, `record_input_names`, `record_output_names`) with default values and validations.
    - Implemented `present` block to check gem presence using `defined?`.
    - Added dependency requirements for Callable and Runner patches.
    - Introduced methods for patching (`patch_callable` and `patch_runner`), enabling instrumentation for Callable and Runner modules.
    - Updated `install` block to conditionally apply patches based on configuration.

--- lib/opentelemetry/instrumentation/servactory/instrumentation.rb
+++ lib/opentelemetry/instrumentation/servactory/instrumentation.rb
@@ -4,20 +4,42 @@ module OpenTelemetry
   module Instrumentation
     module Servactory
       class Instrumentation < OpenTelemetry::Instrumentation::Base
+        instrumentation_version VERSION::STRING
+
+        MINIMUM_VERSION = Gem::Version.new("2.16.0")
+
         install do |_config|
           require_dependencies
+          patch_callable
+          patch_runner if config[:trace_actions]
         end
 
         present do
-          # TODO: Replace true with a definition check of the gem being instrumented
-          # Example: `defined?(::Rack)`
-          true
+          defined?(::Servactory)
         end
+
+        compatible do
+          gem_version = Gem::Version.new(::Servactory::VERSION::STRING)
+          gem_version >= MINIMUM_VERSION
+        end
+
+        option :trace_actions, default: true, validate: :boolean
+        option :record_input_names, default: true, validate: :boolean
+        option :record_output_names, default: true, validate: :boolean
 
         private
 
         def require_dependencies
-          # TODO: Include instrumentation dependencies
+          require_relative "patches/callable"
+          require_relative "patches/runner"
+        end
+
+        def patch_callable
+          ::Servactory::Context::Callable.prepend(Patches::Callable)
+        end
+
+        def patch_runner
+          ::Servactory::Actions::Tools::Runner.prepend(Patches::Runner)
         end
       end
     end
